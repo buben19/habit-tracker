91db7cb6c3cd60aee01512b6a3e553532151b127
Ivo Slanina <ivo.slanina@gmail.com>
2025-11-13 14:07:10 +0100

    add HabitDto class and configure Maven for common module

---
+++ common/src/main/java/cz/buben/learning/habits/common/dto/HabitDto.java
@@ -0,0 +1,10 @@
+package cz.buben.learning.habits.common.dto;
+
+public class HabitDto {
+
+  private Long id;
+  private Long userId;
+  private String name;
+  private String description;
+  private String schedule;
+}
