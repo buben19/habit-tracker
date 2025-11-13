2c917802321c6361964a6339b8baaeea11d5ba98
Ivo Slanina <ivo.slanina@gmail.com>
2025-11-13 14:19:54 +0100

    add Habit entity, repository, and controller for habit management

---
+++ habit-service/src/main/java/cz/buben/learning/habbits/habitservice/repository/HabitRepository.java
@@ -0,0 +1,10 @@
+package cz.buben.learning.habbits.habitservice.repository;
+
+import cz.buben.learning.habbits.habitservice.domain.Habit;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+interface HabitRepository extends JpaRepository<Habit, Long> {
+  List<Habit> findByUserId(Long userId);
+}
