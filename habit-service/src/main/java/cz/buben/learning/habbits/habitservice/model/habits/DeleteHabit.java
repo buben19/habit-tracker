8fc9ee6b19fa77fbd85b0c2ce1f2a06094daf511
Ivo Slanina <ivo.slanina@gmail.com>
2025-11-13 14:27:05 +0100

    add services for habit management: CreateHabit, DeleteHabit, GetHabit, GetHabits, UpdateHabit

---
+++ habit-service/src/main/java/cz/buben/learning/habbits/habitservice/model/habits/DeleteHabit.java
@@ -0,0 +1,11 @@
+package cz.buben.learning.habbits.habitservice.model.habits;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DeleteHabit {
+
+  public void delete(Long id) {
+    throw new UnsupportedOperationException("Not supported yet.");
+  }
+}
---
+++ habit-service/src/main/java/cz/buben/learning/habbits/habitservice/model/habits/UpdateHabit.java
@@ -0,0 +1,12 @@
+package cz.buben.learning.habbits.habitservice.model.habits;
+
+import cz.buben.learning.habbits.habitservice.domain.Habit;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UpdateHabit {
+
+  public Habit update(Habit habit) {
+    throw new UnsupportedOperationException("Not supported yet.");
+  }
+}
