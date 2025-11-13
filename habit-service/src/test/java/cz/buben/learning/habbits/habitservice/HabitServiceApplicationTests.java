482772d3e8a3338eb99a88d1545a75f0f687897d
Ivo Slanina <ivo.slanina@unicorn.com>
2025-11-13 13:51:16 +0100

    initialize habit-service module with Maven setup and basic application structure

---
+++ habit-service/src/test/java/cz/buben/learning/habbits/habitservice/HabitServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.buben.learning.habbits.habitservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
