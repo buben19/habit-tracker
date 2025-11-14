e3f16748e5ca318b9aa232163323b4e9795a408c
Ivo Slanina <ivo.slanina@gmail.com>
2025-11-14 09:39:20 +0100

    initialize checkin service with Maven setup and basic application structure

---
+++ checkin-service/src/test/java/cz/buben/learning/habbits/checkinservice/CheckinServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.buben.learning.habbits.checkinservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckinServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
