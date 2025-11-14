f3addd86af0e4d328491191a2529e4ba6afa3556
Ivo Slanina <ivo.slanina@gmail.com>
2025-11-14 09:29:50 +0100

    implement CRUD operations for habit management: Create, Delete, Get, GetAll, and Update

--- habit-service/src/main/java/cz/buben/learning/habbits/habitservice/model/habits/CreateHabit.java
+++ habit-service/src/main/java/cz/buben/learning/habbits/habitservice/model/habits/CreateHabit.java
@@ -1,12 +1,19 @@
 package cz.buben.learning.habbits.habitservice.model.habits;
 
 import cz.buben.learning.habbits.habitservice.domain.Habit;
+import cz.buben.learning.habbits.habitservice.repository.HabitRepository;
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class CreateHabit {
 
+  private final HabitRepository habitRepository;
+
+  @Transactional
   public Habit create(Habit habit) {
-    throw new UnsupportedOperationException("Not supported yet.");
+    return habitRepository.save(habit);
   }
 }
--- habit-service/src/main/java/cz/buben/learning/habbits/habitservice/model/habits/GetHabit.java
+++ habit-service/src/main/java/cz/buben/learning/habbits/habitservice/model/habits/GetHabit.java
@@ -1,12 +1,20 @@
 package cz.buben.learning.habbits.habitservice.model.habits;
 
 import cz.buben.learning.habbits.habitservice.domain.Habit;
+import cz.buben.learning.habbits.habitservice.repository.HabitRepository;
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class GetHabit {
 
+  private final HabitRepository habitRepository;
+
+  @Transactional
   public Habit get(Long id) {
-    throw new UnsupportedOperationException("Not supported yet.");
+    return habitRepository.findById(id)
+        .orElseThrow();
   }
 }
