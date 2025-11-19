3409377eafb0b3daa7b1c735ea86b9045e1fcf41
Ivo Slanina <ivo.slanina@gmail.com>
2025-11-19 13:47:41 +0100

    feat: add Spring Cloud Config and Eureka server setup with initial configuration files

---
+++ config-server/src/test/java/cz/buben/learning/habbits/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.buben.learning.habbits.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
---
+++ discovery/src/test/java/cz/buben/learning/habbits/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.buben.learning.habbits.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
