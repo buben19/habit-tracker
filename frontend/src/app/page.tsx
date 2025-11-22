815e3428592f8e9858c1c6f8362a0645f4ec72fc
Ivo Slanina <ivo.slanina@gmail.com>
2025-11-22 13:19:38 +0100

    feat: restructure layout to render NavBar outside KeycloakProvider and update Nav.Link hrefs

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -31,7 +31,6 @@ export default function HomePage() {
       method: "POST",
       body: JSON.stringify({ habitId })
     });
-    alert("Marked!");
   }
 
   return (
--- frontend/src/ui/NavBar.tsx
+++ frontend/src/ui/NavBar.tsx
@@ -13,7 +13,7 @@ export const NavBar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="#link">Link</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
@@ -21,7 +21,7 @@ export const NavBar = () => {
                 Another action
               </NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
+              <NavDropdown.Divider/>
               <NavDropdown.Item href="#action/3.4">
                 Separated link
               </NavDropdown.Item>
