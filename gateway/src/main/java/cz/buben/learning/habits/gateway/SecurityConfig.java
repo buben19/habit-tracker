44f53ac98ec15204a979097132031b7e35369b8a
Ivo Slanina <ivo.slanina@gmail.com>
2025-11-27 18:42:03 +0100

    feat: add management endpoints and Prometheus metrics configuration

--- gateway/src/main/java/cz/buben/learning/habits/gateway/SecurityConfig.java
+++ gateway/src/main/java/cz/buben/learning/habits/gateway/SecurityConfig.java
@@ -20,7 +20,7 @@ import java.util.List;
 public class SecurityConfig {
 
   @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) {
     http
         .csrf(AbstractHttpConfigurer::disable)
         .cors(Customizer.withDefaults())
