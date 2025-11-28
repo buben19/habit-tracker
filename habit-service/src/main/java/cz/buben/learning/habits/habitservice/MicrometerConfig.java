e7b90b99a57189fe1bd401977a747d1c2fc1f408
Ivo Slanina <ivo.slanina@gmail.com>
2025-11-28 15:07:04 +0100

    feat: add Micrometer configuration and dependencies for Feign integration

---
+++ habit-service/src/main/java/cz/buben/learning/habits/habitservice/MicrometerConfig.java
@@ -0,0 +1,16 @@
+package cz.buben.learning.habits.habitservice;
+
+import feign.Capability;
+import feign.micrometer.MicrometerCapability;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MicrometerConfig {
+
+  @Bean
+  public Capability capability(MeterRegistry registry) {
+    return new MicrometerCapability(registry);
+  }
+}
