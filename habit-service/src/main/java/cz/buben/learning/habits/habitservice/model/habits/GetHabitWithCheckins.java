e8ea993dfd4f0a1a9f6c99cae52562ec397a749f
Ivo Slanina <ivo.slanina@gmail.com>
2025-12-02 08:53:57 +0100

    feat: enable virtual threads for asynchronous task execution and update check-in fetching

--- habit-service/src/main/java/cz/buben/learning/habits/habitservice/model/habits/GetHabitWithCheckins.java
+++ habit-service/src/main/java/cz/buben/learning/habits/habitservice/model/habits/GetHabitWithCheckins.java
@@ -7,10 +7,14 @@ import cz.buben.learning.habits.habitservice.client.UserClient;
 import cz.buben.learning.habits.habitservice.domain.Habit;
 import cz.buben.learning.habits.habitservice.mapping.HabitMapper;
 import cz.buben.learning.habits.habitservice.repository.HabitRepository;
+import io.micrometer.context.ContextSnapshotFactory;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.task.SimpleAsyncTaskSchedulerBuilder;
 import org.springframework.core.task.AsyncTaskExecutor;
+import org.springframework.core.task.TaskDecorator;
+import org.springframework.scheduling.concurrent.SimpleAsyncTaskScheduler;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -38,7 +42,7 @@ public class GetHabitWithCheckins {
         () -> new RuntimeException("Cannot get habits - no authenticated user found"));
     UserDto userById = userClient.findUserById(userId);
     List<Habit> habits = habitRepository.findByUserId(userId);
-    Map<Long, List<CheckinDto>> checkinsForHabits = getCheckinsForHabitsSync(habits);
+    Map<Long, List<CheckinDto>> checkinsForHabits = getCheckinsForHabits(habits);
     List<HabitCompleteResponse> habitCompleteResponses = new ArrayList<>();
     habits.forEach(habit -> {
       HabitDto habitDto = habitMapper.entityToDto(habit);
@@ -55,38 +59,44 @@ public class GetHabitWithCheckins {
         .build();
   }
 
-  private Map<Long, List<CheckinDto>> getCheckinsForHabitsSync(List<Habit> habits) {
-    return habits.stream()
-        .collect(Collectors.toMap(
-            Habit::getId,
-            habit -> checkinClient.getCheckinsByHabitId(habit.getId())
-        ));
+  private Map<Long, List<CheckinDto>> getCheckinsForHabits(List<Habit> habits) {
+    TaskDecorator taskDecorator = taskDecorator();
+    try (SimpleAsyncTaskScheduler simpleAsyncTaskScheduler = new SimpleAsyncTaskSchedulerBuilder()
+        .taskDecorator(taskDecorator)
+        .build()) {
+
+      Map<Long, Future<List<CheckinDto>>> futureMap = habits.stream()
+          .collect(
+              Collectors.toMap(
+                  Habit::getId,
+                  habit -> CompletableFuture.supplyAsync(
+                      () -> checkinClient.getCheckinsByHabitId(habit.getId()),
+                      simpleAsyncTaskScheduler
+                  )
+              )
+          );
+
+      return futureMap.entrySet().stream()
+          .collect(
+              Collectors.toMap(
+                  Map.Entry::getKey,
+                  entry -> {
+                    try {
+                      return entry.getValue().get();
+                    } catch (Exception e) {
+                      throw new RuntimeException("Failed to get checkins for habit id: " + entry.getKey(), e);
+                    }
+                  }
+              )
+          );
+    }
   }
 
-  private Map<Long, List<CheckinDto>> getCheckinsForHabits(List<Habit> habits) {
-    Map<Long, Future<List<CheckinDto>>> futureMap = habits.stream()
-        .collect(
-            Collectors.toMap(
-                Habit::getId,
-                habit -> CompletableFuture.supplyAsync(
-                    () -> checkinClient.getCheckinsByHabitId(habit.getId()),
-                    asyncTaskExecutor
-                )
-            )
-        );
-
-    return futureMap.entrySet().stream()
-        .collect(
-            Collectors.toMap(
-                Map.Entry::getKey,
-                entry -> {
-                  try {
-                    return entry.getValue().get();
-                  } catch (Exception e) {
-                    throw new RuntimeException("Failed to get checkins for habit id: " + entry.getKey(), e);
-                  }
-                }
-            )
-        );
+  private TaskDecorator taskDecorator() {
+    return runnable -> ContextSnapshotFactory
+        .builder()
+        .build()
+        .captureAll()
+        .wrap(runnable);
   }
 }
