001fc5a2842b8390e15e0644c512af98e6a012be
Ivo Slanina <ivo.slanina@gmail.com>
2025-12-16 16:07:49 +0100

    feat: refactor NewHabitPage to manage habit state and include description field

--- frontend/src/app/habit/new/page.tsx
+++ frontend/src/app/habit/new/page.tsx
@@ -5,10 +5,20 @@ import { useRouter } from "next/navigation";
 import { apiFetch } from "@/lib/api";
 import { useAuth } from "@/ui/KeycloakProvider";
 import { Button, Form } from "react-bootstrap";
+import * as React from "react";
+
+type NewHabit = {
+  name?: string | undefined;
+  description?: string | undefined;
+  schedule: "DAILY" | "WEEKLY" | "MONTHLY" | "YEARLY";
+}
 
 export default function NewHabitPage() {
-  const [name, setName] = useState("");
-  const [schedule, setSchedule] = useState("DAILY");
+  const [habit, updateHabit] = useState<NewHabit>({
+    name: undefined,
+    description: undefined,
+    schedule: "DAILY",
+  });
   const router = useRouter();
   const { initialized, authenticated, login, logout, token } = useAuth();
 
@@ -16,9 +26,16 @@ export default function NewHabitPage() {
     e.preventDefault();
     await apiFetch("/habits", token, {
       method: "POST",
-      body: JSON.stringify({ name, schedule })
+      body: JSON.stringify(habit)
     });
     router.push("/");
+  }
+
+  const handleChange = (e: { target: { name: string; value: string; }; }) => {
+    updateHabit({
+      ...habit,
+      [e.target.name]: e.target.value
+    });
   }
 
   return (
@@ -27,12 +44,15 @@ export default function NewHabitPage() {
       <Form onSubmit={submit}>
         <Form.Group>
           <Form.Label>Habit name</Form.Label>
-          <Form.Control type="text" value={name} onChange={e => setName(e.target.value)} placeholder="Enter habit name" />
-          <Form.Text>adsd</Form.Text>
+          <Form.Control type="text" name="name" value={habit.name} onChange={handleChange} placeholder="Enter habit name" />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Habit description</Form.Label>
+          <Form.Control type="text" name="description" value={habit.description} onChange={handleChange} placeholder="Enter habit description" />
         </Form.Group>
         <Form.Group>
           <Form.Label>Schedule</Form.Label>
-          <Form.Select value={schedule} onChange={e => setSchedule(e.target.value)}>
+          <Form.Select name="schedule" value={habit.schedule} onChange={handleChange}>
             <option value="DAILY">DAILY</option>
             <option value="WEEKLY">WEEKLY</option>
             <option value="MONTHLY">MONTHLY</option>
